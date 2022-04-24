a2ae1a4229b2ef76dbab415134ee7c4b83757197
xavialfaro <nxb.developer@gmail.com>
2022-04-23 22:20:14 -0500

    added services for authentication

---
+++ services/auth.ts
@@ -0,0 +1,27 @@
+import { supabase } from './config'
+/**
+ * Authentication with provider
+ */
+type Provider = 'google' | 'discord'
+
+export const signInWithProvider = async (provider: Provider) => {
+  try {
+    const { user, error } = await supabase.auth.signIn({
+      provider: provider
+    })
+    if (error) throw new Error('An error ocurred during authentication')
+    return user
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+/** Logout */
+export const logout = async () => {
+  try {
+    const { error } = await supabase.auth.signOut()
+    if (error) throw new Error('An error ocurred during logout')
+  } catch (error) {
+    console.error(error)
+  }
+}
