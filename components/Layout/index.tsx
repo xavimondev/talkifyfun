e36acc57df924bcc7dcf38a3b0165f0a9715ab4c
xavialfaro <nxb.developer@gmail.com>
2022-04-24 17:58:00 -0500

    add layout component

---
+++ components/Layout/index.tsx
@@ -0,0 +1,16 @@
+import { Box, Flex } from '@chakra-ui/react'
+
+type Props = {
+  children: React.ReactNode
+}
+const Layout = ({ children }: Props) => {
+  return (
+    <>
+      <Flex w='100vw' h='100vh' justifyContent='center' alignItems='center'>
+        {children}
+      </Flex>
+    </>
+  )
+}
+
+export default Layout
--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -2,12 +2,15 @@ import type { NextPage } from 'next'
 
 import Header from 'components/Header'
 import Dashboard from 'components/Dashboard'
+import Layout from 'components/Layout'
 
 const Home: NextPage = () => {
   return (
     <>
       <Header title='Dashboard' content='Welcome to dashboard xavimon.dev 😊' />
-      <Dashboard />
+      <Layout>
+        <Dashboard />
+      </Layout>
     </>
   )
 }
