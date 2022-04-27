699de43295b76b8f52ec61e03cf64803dcd64a9c
xavialfaro <nxb.developer@gmail.com>
2022-04-26 22:01:47 -0500

    add custom hook useTrack

--- components/RoomDetails/Member.tsx
+++ components/RoomDetails/Member.tsx
@@ -1,14 +1,7 @@
-import { useEffect, useRef, useState } from 'react'
 import { Box, Flex } from '@chakra-ui/react'
-import {
-  AudioTrackPublication,
-  LocalAudioTrack,
-  LocalVideoTrack,
-  Participant,
-  RemoteAudioTrack,
-  RemoteVideoTrack,
-  VideoTrackPublication
-} from 'twilio-video'
+import { Participant } from 'twilio-video'
+
+import useTrack from 'hooks/useTrack'
 
 import ControlsRoom from './ControlsRoom'
 
@@ -16,72 +9,9 @@ type Props = {
   member: Participant
 }
 
-type TrackPublication = VideoTrackPublication | AudioTrackPublication
-type MediaTrack = LocalVideoTrack | RemoteVideoTrack | LocalAudioTrack | RemoteAudioTrack | null
-
 const Member = ({ member }: Props) => {
   // console.log(member)
-  const [videoTracks, setVideoTracks] = useState<any[]>([])
-  const [audioTracks, setAudioTracks] = useState<any[]>([])
-
-  const videoRef = useRef<HTMLVideoElement>(null)
-  const audioRef = useRef<HTMLAudioElement>(null)
-
-  const trackpubsToTracks = (trackMap: Map<string, TrackPublication>): MediaTrack[] =>
-    Array.from(trackMap.values())
-      .map((publication) => publication.track)
-      .filter((track) => track !== null)
-
-  useEffect(() => {
-    const trackSubscribed = (track: any) => {
-      if (track.kind === 'video') {
-        setVideoTracks((videoTracks) => [...videoTracks, track])
-      } else {
-        setAudioTracks((audioTracks) => [...audioTracks, track])
-      }
-    }
-
-    const trackUnsubscribed = (track: any) => {
-      if (track.kind === 'video') {
-        setVideoTracks((videoTracks) => videoTracks.filter((v) => v !== track))
-      } else {
-        setAudioTracks((audioTracks) => audioTracks.filter((a) => a !== track))
-      }
-    }
-
-    setVideoTracks(trackpubsToTracks(member.videoTracks))
-    setAudioTracks(trackpubsToTracks(member.audioTracks))
-
-    member.on('trackSubscribed', trackSubscribed)
-    member.on('trackUnsubscribed', trackUnsubscribed)
-
-    return () => {
-      setVideoTracks([])
-      setAudioTracks([])
-      member.removeAllListeners()
-    }
-  }, [member])
-
-  useEffect(() => {
-    const videoTrack = videoTracks[0]
-    if (videoTrack) {
-      videoTrack.attach(videoRef.current)
-      return () => {
-        videoTrack.detach()
-      }
-    }
-  }, [videoTracks])
-
-  useEffect(() => {
-    const audioTrack = audioTracks[0]
-    if (audioTrack) {
-      audioTrack.attach(videoRef.current)
-      return () => {
-        audioTrack.detach()
-      }
-    }
-  }, [audioTracks])
-
+  const { audioRef, videoRef } = useTrack(member)
   return (
     <>
       <Flex
